c6aa7a1e2d2a5a4f75058c12a80296169d9a31eb
Marta Kule <marta.kule@gmail.com>
2018-04-01 09:31:52 -0400

    Placeholder props

--- client/src/features/database-feedback/database-feedback.component.js
+++ client/src/features/database-feedback/database-feedback.component.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Toast from "../../ui-kit/toast";
 
-export const DatabaseFeedback = (props) => {
-	if (props.success > 0) {
- 	 	return <Toast type="success">Thank you for submitting a review for The Delights of China!</Toast>
+export const DatabaseFeedback = (placeholder) => {
+	if (placeholder) {
+ 	 	return <Toast type="success">Thank you for submitting a review for [Location Name]!</Toast>
 	} else {
 		return <Toast type="error">Sorry, there seems to be a network error. Make sure you're online and try submitting again.</Toast>  
 	}
--- client/src/features/database-feedback/database-feedback.container.js
+++ client/src/features/database-feedback/database-feedback.container.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { DatabaseFeedback } from "./database-feedback.component";
 
 const mapStateToProps = state => ({
-	success:state.locationRatingReducer.features
+	
 });
 
 export const DatabaseFeedbackContainer = connect(mapStateToProps)(DatabaseFeedback);
